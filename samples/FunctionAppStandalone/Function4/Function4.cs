255b309b6ce043cb104a0690f2ab77f68becc3fc
David Brazier <david.brazier@outlook.com>
2021-03-19 02:01:51 +0000

    Experiment for https://github.com/Azure/azure-functions-dotnet-worker/issues/297

---
+++ samples/FunctionAppStandalone/Function4/Function4.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp
+{
+    public static class Function4
+    {
+        [Function("Function4")]
+        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req, FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("FunctionApp.Function4");
+
+            logger.LogInformation("message logged");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            response.Headers.Add("Date", "Mon, 18 Jul 2016 16:06:00 GMT");
+            response.Headers.Add("Content", "Content - Type: text / html; charset = utf - 8");
+
+            response.WriteString("Welcome to .NET 5!!");
+
+            return response;
+        }
+    }
+
+}
---
+++ samples/FunctionAppStandalone/Program.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FunctionApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+// #if DEBUG
+//          Debugger.Launch();
+// #endif
+            //<docsnippet_startup>
+            var host = new HostBuilder()
+                //<docsnippet_middleware>
+                .ConfigureFunctionsWorkerDefaults()
+                //</docsnippet_middleware>
+                //<docsnippet_dependency_injection>
+                .ConfigureServices(s =>
+                {
+                    s.AddSingleton<IHttpResponderService, DefaultHttpResponderService>();
+                })
+                //</docsnippet_dependency_injection>
+                .Build();
+            //</docsnippet_startup>
+
+            //<docsnippet_host_run>
+            await host.RunAsync();
+            //</docsnippet_host_run>
+        }
+    }
+}
